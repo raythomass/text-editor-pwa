ad1b53cfac7a2ad9978bcfedecd5087d1c729edf
Ray Thomas <rthomas5678@gmail.com>
2024-01-14 20:54:56 -0700

    added changes to src js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,31 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const contactDb = await openDB('contact', 1);
+  const tx = await contactDb.transaction('contact', 'readwrite');
+  const store = await tx.objectStore('contact');
+  const request = await store.put(content);
+
+  const result = await request;
+  console.log('request.value', result);
+  return result;
+
+  // console.error('putDb not implemented');
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const contactDb = await openDB('contact', 1);
+  const tx = await contactDb.transaction('contact', 'readonly');
+  const store = await tx.objectStore('contact');
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('request.value', result);
+  return result
+
+  // console.error('getDb not implemented');
+}
 
 initdb();
