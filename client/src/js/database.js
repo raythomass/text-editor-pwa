cd85a12f31986008d7cbc4b4951559747270822e
Ray Thomas <rthomas5678@gmail.com>
2024-01-15 19:13:06 -0700

    researched and fixed known problems with dev deployment

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,10 +14,10 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const contactDb = await openDB('contact', 1);
-  const tx = await contactDb.transaction('contact', 'readwrite');
-  const store = await tx.objectStore('contact');
-  const request = await store.put(content);
+  const contactDb = await openDB('jate', 1);
+  const tx = await contactDb.transaction('jate', 'readwrite');
+  const store = await tx.objectStore('jate');
+  const request = await store.put({ id: 1, value: content });
 
   const result = await request;
   console.log('request.value', result);
@@ -28,9 +28,9 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const contactDb = await openDB('contact', 1);
-  const tx = await contactDb.transaction('contact', 'readonly');
-  const store = await tx.objectStore('contact');
+  const contactDb = await openDB('jate', 1);
+  const tx = await contactDb.transaction('jate', 'readonly');
+  const store = await tx.objectStore('jate');
   const request = store.getAll();
 
   const result = await request;
